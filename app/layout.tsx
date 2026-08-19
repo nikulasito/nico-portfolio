3a2507ec2c9c1d3a077362e92eb4dccae5d49dc3
Nico <n.alumbro@gmail.com>
2026-08-19 09:12:49 +0800

    updated google tag env

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,36 +20,27 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
-  const googleTagManagerId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
+  const googleTagId = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID;
 
   return (
     <html lang="en" suppressHydrationWarning>
-      {googleTagManagerId ? (
+      {googleTagId ? (
         <head>
-          <Script id="google-tag-manager" strategy="afterInteractive">
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-tag" strategy="afterInteractive">
             {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${googleTagManagerId}');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${googleTagId}');
             `}
           </Script>
         </head>
       ) : null}
-      <body suppressHydrationWarning>
-        {googleTagManagerId ? (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        ) : null}
-        {children}
-      </body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
